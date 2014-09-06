44589d6785a2f371a9564013c87fec48f993db84
Bryce Kerley <bkerley@brycekerley.net>
2014-09-06 17:05:43 -0400

    failing spec

--- spec/its_spelled_referrer_spec.rb
+++ spec/its_spelled_referrer_spec.rb
@@ -1,5 +1,27 @@
+require 'rack/mock'
+require 'rack/its/spelled/referrer'
+
 describe Rack::Its::Spelled::Referrer do
-  it 'lints without issue'
+  let(:app) do
+    double('app')
+  end
+
+  let(:url){ 'http://example.invalid' }
+
+  subject(:request) do
+    Rack::MockRequest.new described_class.new app
+  end
+
+  before :each do
+    allow(app).to receive(:call).and_return [200, {}, ['ok']]
+  end
+
+  it 'lints without issue' do
+    resp = nil
+    expect{ resp = subject.get url, lint: true }.to_not raise_error
+    expect(resp.errors).to be_empty
+  end
+
   it 'replaces the Referer header with Referrer'
   it "isn't case sensitive"
 end
