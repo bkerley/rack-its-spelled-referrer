7ee15c3b25f6da997e8b35bf11bc05e8af41b403
Bryce Kerley <bkerley@brycekerley.net>
2014-09-06 17:06:35 -0400

    spike!

--- lib/rack/its/spelled/referrer.rb
+++ lib/rack/its/spelled/referrer.rb
@@ -4,6 +4,29 @@ module Rack
   module Its
     module Spelled
       class Referrer
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          referer = find_referer env
+
+          return @app.call env unless referer
+
+          referred_env = fix_spelling env, referer
+
+          return @app.call referred_env
+        end
+
+        private
+        
+        def find_referer(env)
+          return env['HTTP_REFERER']
+        end
+
+        def fix_spelling(env, referer)
+          env['HTTP_REFERRER'] = referer
+        end
       end
     end
   end
