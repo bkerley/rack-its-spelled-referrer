6bd1daadc18c222999e390e7b97ecfff7242ce22
Bryce Kerley <bkerley@brycekerley.net>
2014-09-06 17:26:04 -0400

    spec out replacement behavior

--- spec/its_spelled_referrer_spec.rb
+++ spec/its_spelled_referrer_spec.rb
@@ -3,17 +3,15 @@ require 'rack/its/spelled/referrer'
 
 describe Rack::Its::Spelled::Referrer do
   let(:app) do
-    double('app')
+    spy('app', call: [200, {}, ['ok']])
   end
 
   let(:url){ 'http://example.invalid' }
 
-  subject(:request) do
-    Rack::MockRequest.new described_class.new app
-  end
+  let(:middleware){ described_class.new app }
 
-  before :each do
-    allow(app).to receive(:call).and_return [200, {}, ['ok']]
+  subject(:request) do
+    Rack::MockRequest.new middleware
   end
 
   it 'lints without issue' do
@@ -22,6 +20,9 @@ describe Rack::Its::Spelled::Referrer do
     expect(resp.errors).to be_empty
   end
 
-  it 'replaces the Referer header with Referrer'
-  it "isn't case sensitive"
+  it 'replaces the Referer header with Referrer' do
+    resp = subject.get(url, lint: true, 'HTTP_REFERER' => url)
+    expect(resp.errors).to be_empty
+    expect(app).to have_received(:call).with(hash_including('HTTP_REFERER' => url, 'HTTP_REFERRER' => url))
+  end
 end
